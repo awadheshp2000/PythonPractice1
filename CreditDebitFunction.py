af31e1d74080a8b277e82509888aac8568dc6f47
Awadhesh Singh <awadheshp2000@gmail.com>
2020-04-02 23:58:42 +0530

    commit

---
+++ CreditDebitFunction.py
@@ -0,0 +1,18 @@
+from typing import TypeVar
+import
+
+Class Cls_CustomerTransactions:
+
+    Conn =
+
+    def account_balance (accountnumber:float)->  float:
+        if accountnumber != 0:
+            return 10,000
+    def debit_credit (accountnumber:float, amount:float, debit_or_credit: bool) -> bool
+        if accountntnumber !=0
+
+
+
+_
+
+
