13f348ddc044d21a75d8afcbdba316430cb340b6
awadhesh <awadheshp2000@gmail.com>
2020-04-11 23:35:31 +0100

    updated

--- venv/test_Patient_UI.py
+++ venv/test_Patient_UI.py
@@ -4,7 +4,7 @@ def test_sum():
     number1: float = 400
     number2: float =800
     number3: float
-    number3 = test_sum(number2 + number2)
+    number3 = test_sum(number1 + number2)
     assert number3 == 1200
 
 
