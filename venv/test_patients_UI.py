cdd50c2a32dff5bd36592acbd264f34b42a5234b
awadhesh <awadheshp2000@gmail.com>
2020-04-12 01:32:20 +0100

    updated

--- venv/test_patients_UI.py
+++ venv/test_patients_UI.py
@@ -2,10 +2,11 @@ from typing import TypeVar
 import Patient_Prediction
 
 class test_patient_prediction:
-    def test_sum(self):
+    def test_sum():
         number1 = 400
         number2 =800
         number3 = 0
         Patient_Predict1 = Patient_Prediction.Patient_Predict()
         number3 = Patient_Predict1.sum(number1 , number2)
         assert number3 == 1200
+
