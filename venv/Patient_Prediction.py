1ed178a99c0da106629b7e2530ee0206a47d3f35
awadhesh <awadheshp2000@gmail.com>
2020-04-12 01:41:57 +0100

    updated

--- venv/Patient_Prediction.py
+++ venv/Patient_Prediction.py
@@ -1,8 +1,8 @@
 from typing import TypeVar
 
-class Patient_Predict:
-    def sum (a: float,b:float)-> float:
-        number2: float
-        number2:float = a+b
-        return number2
+#class Patient_Predict:
+def sum (a: float,b:float)-> float:
+    number2: float
+    number2:float = a+b
+    return number2
 
